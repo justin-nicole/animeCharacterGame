971a102e1a92422b94bd13a788083ea95bf8b10e
Nicole de Guzman <nicoledeguzman@Nicoles-MacBook-Air-2.local>
2021-04-10 01:08:34 -0400

    landingpage and footer components

--- src/Footer.js
+++ src/Footer.js
@@ -1,1 +1,9 @@
+const Footer = () => {
+    return(
+        <footer>
+            <p>Copyright 2021 © Juno College</p>
+        </footer>
+    )
+}
+
 export default Footer;
--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,1 +1,28 @@
+import './LandingPage.css'
+
+const LandingPage = () => {
+    return(
+        <header className="LandingPage">
+            <h1>ANIMEANIMEANIME</h1>
+            <div className="grid">
+                <div className="item item1">1</div>
+                <div className="item item2">2</div>
+                <div className="item item3">3</div>
+                <div className="item item4">4</div>
+                <div className="item item5">5</div>
+                <div className="item item6">6</div>
+                <div className="item item7">7</div>
+                <div className="item item8">8</div>
+                <div className="item item9">9</div>
+                <div className="item item10">10</div>
+                <div className="item item11">11</div>
+                <div className="item item12">12</div>
+                <div className="item item13">13</div>
+                <div className="item item14">14</div>
+                <div className="item item15">15</div>
+            </div>
+        </header>
+    )
+}
+
 export default LandingPage;
