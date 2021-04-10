9492dcdd0531856f911ae71b7e3080677b28554f
Nicole de Guzman <nicoledeguzman@Nicoles-MacBook-Air-2.local>
2021-04-09 22:59:11 -0400

    created componentsss

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import './App.css';
+import {useState, useEffect} from 'react';
 
 function App() {
+  const [userInput, setUserInput] = useState('');
+  // const [animeCharacters, setAnimeCharacters] = useState([]);
+  let animeCharacters= [];
+  const [currentCharacter, setCurrentCharacter] = useState({});
+ 
+  const getAnimeCharacters = () =>{
+    const proxiedUrl = 'https://api.jikan.moe/v3/top/characters/'
+    const url = new URL("https://proxy.hackeryou.com");
+    url.search = new URLSearchParams({
+        reqUrl: proxiedUrl
+    })
+    fetch(url)
+    .then(res => res.json())
+    .then((jsonResp) =>{
+      animeCharacters = jsonResp.top
+      console.log(animeCharacters);
+    })
+  }
+
+ 
+  getAnimeCharacters();
+
+  
+
   return (
     <div className="App">
       <h1>ANIMEANIMEANIME</h1>
+      <form>
+      <input type="text"></input>
+      </form>
     </div>
+    
   );
 }
 
